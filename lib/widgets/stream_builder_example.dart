fecbad282aa25cad8a60b33c8b6355406f50a20a
MNI <mni@eqsystem.pl>
2022-10-03 15:36:08 +0200

    papaj

--- lib/widgets/stream_builder_example.dart
+++ lib/widgets/stream_builder_example.dart
@@ -56,7 +56,7 @@ class _StreamBuilderExampleState extends State<StreamBuilderExample> {
     );
   }
 
-  Text onConnection(AsyncSnapshot<int> snapshot) {
+  Widget onConnection(AsyncSnapshot<int> snapshot) {
     if (snapshot.hasError) {
       return const Text('Error');
     } else if (snapshot.hasData) {
@@ -66,13 +66,29 @@ class _StreamBuilderExampleState extends State<StreamBuilderExample> {
     return const Text('Empty data');
   }
 
-  Text onHasData(AsyncSnapshot<int> snapshot) {
-    return Text(
-      snapshot.data.toString(),
-      style: const TextStyle(
-        color: Colors.red,
-        fontSize: 40,
-      ),
+  Widget onHasData(AsyncSnapshot<int> snapshot) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          snapshot.data.toString(),
+          style: const TextStyle(
+            color: Colors.red,
+            fontSize: 40,
+          ),
+        ),
+        if (snapshot.data == 2137)
+          Row(
+            children: [
+              const SizedBox(width: 10),
+              SizedBox(
+                child: Image.network('https://www.sp-klucze.pl/wp-content/uploads/2016/11/Jan-Pawe%C5%82-II.jpg'),
+                height: 80,
+                width: 80,
+              ),
+            ],
+          ),
+      ],
     );
   }
 
@@ -82,12 +98,13 @@ class _StreamBuilderExampleState extends State<StreamBuilderExample> {
     streamController = StreamController<int>();
     await Future<void>.delayed(const Duration(seconds: 3));
 
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 2137; i++) {
       streamController.add(i);
-      await Future<void>.delayed(const Duration(seconds: 1));
+      await Future<void>.delayed(const Duration(microseconds: 1));
       setState(() {});
     }
 
+    await Future<void>.delayed(const Duration(seconds: 2));
     streamController.close();
     await Future<void>.delayed(const Duration(seconds: 2));
     counting = false;
